6390c1cd559f27a786c84b96b6fd203d6f89fc32
chrs.ppls <chrs_ppls@yahoo.com>
2018-08-27 18:45:23 -0400

    Second Commit

--- contracts/InvestmentScreen.sol
+++ contracts/InvestmentScreen.sol
@@ -1,161 +1,145 @@
 pragma solidity ^0.4.24;
 
-contract InvestmentScreen {
+import "./SafeMath.sol";
+import "./Ownable.sol";
+
+contract InvestmentScreen is Ownable{
+  using SafeMath for uint256;
 
 address owner;
 
 uint memberNumber;
 uint opportunityNumber;
 
-struct Member {
-  address account;
-  uint id;
-  string firstName;
-  string lastName;
-  uint memberType;
-}
 
 // Struct with investment characteristics
 struct InvestmentOpportunity{
   uint opportunityId;
   address investee;
-  address screener;
-  address investor;
+  bool funded;
+  string status;
   string opportunityName;
   string opportunityDescription;
-  uint currentFunding;
   uint fundingTarget;
-  uint investmentScore;
-  uint opportunityStatus;
-  uint[] scoreArray;
-}
-
-// types of member categories
-enum memberType {
-  Investor,
-  Assessor,
-  Investee
+  uint screeners;
+  address investor;
+  address endorsement1;
+  address endorsement2;
+  address endorsement3;
 }
 
 // status states for investment opportunities
 enum opportunityStatus {
   Active,
-  Funded
+  Funded,
+  Closed
 }
 
 enum ranking {
   Strong,
   Good,
   Average,
-  Poor,
-  Terrible
+  Poor
 }
 
-// mapping of site member types
-mapping (uint => Member) public members;
-
 // mapping of available opportunities
+
 mapping (uint => InvestmentOpportunity) public opportunities;
+
+
+mapping (uint => address) public opportunityToOwner;
+mapping (address => uint) public opportunitiesOwned;
+
+mapping (address => uint) public opportunitiesAssessed;
+
+InvestmentOpportunity[] public investmentOpportunities;
+
+string[] statusDescriptions = new string[] (3);
 
 // contract constuctor
 constructor() public {
   owner = msg.sender;
   memberNumber = 0;
   opportunityNumber = 0;
+  statusDescriptions[0] =   "Investment needs additional screening";
+  statusDescriptions[1] =   "Investment has been screened and is ready for funding";
+  statusDescriptions[2] =   "Investment has been screened and has been funded";
 }
 
 // event for adding a member
-event MemberAdded(
-  address _account,
-  uint indexed _id,
-  string _firstName,
-  string _lastName,
-  uint _memberType
+event CurrentStatus(
+  address _endorsement1,
+  address _endorsement2,
+  address _endorsement3
   );
 
-  event OpportunitySnapshot(
+event OpportunitySnapshot(
     uint _opportunityId,
     address _investee,
-    address _screener,
-    address _investor,
+    bool _funded,
+    string _status,
     string _opportunityName,
     string _opportunityDescription,
-    uint _currentFunding,
     uint _fundingTarget,
-    uint _investmentScore,
-    uint _opportunityStatus,
-    /* mapping (address => uint) public screeners */
-    uint _scoreArray
+    uint _screeners,
+    uint _investor,
+    address _endorsement1,
+    address _endorsement2,
+    address _endorsement3
     );
 
+event OpportunityEndorsed(
+  address _screener,
+  string _opportunityName
+  );
 
-// Enroll new member accounts
-function addMember(
-  string _firstName,
-  string _lastName,
-  uint _memberType
-  ) public {
-    /* emit MemberAdded(memberNumber); */
-    memberNumber++;
-    members[memberNumber] = Member({
-      account: msg.sender,
-      id: memberNumber,
-      firstName: _firstName,
-      lastName: _lastName,
-      memberType: _memberType});
+event OpportunityFunded(
+  uint _opportunityId,
+  address _investee,
+  address _investor,
+  string _opportunityName,
+  uint _fundingTarget);
 
-    emit MemberAdded(msg.sender, memberNumber, _firstName, _lastName, _memberType);
-  }
-
-// Get the number of members by member type
-function getNumberOfMembers()
-public view returns(uint){
-  return memberNumber;
-}
-
-// View users (not tested)
-function viewMember(uint _member) view public returns(uint, string, string, uint) {
-  Member storage member = members[_member];
-  return(member.id, member.firstName, member.lastName, member.memberType);
-}
-
-// add investment opportunities
-function postInvestment(
+function createOpportunity(
   string _opportunityName,
   string _opportunityDescription,
   uint _fundingTarget
   ) public {
     opportunityNumber++;
+    string storage status = statusDescriptions[0];
+    opportunities[opportunityNumber] = InvestmentOpportunity(
+      /* opportunityId: */ opportunityNumber,
+      /* investee: */ msg.sender,
+      /* funded: */ false,
+      /* status: */ status,
+      /* opportunityName: */ _opportunityName,
+      /* opportunityDescription: */ _opportunityDescription,
+      /* fundingTarget: */ _fundingTarget,
+      /* screeners: */ 0,
+      /* investor: */ 0x0,
+      /* endorsement1: */ 0x0,
+      /* endorsement2: */ 0x0,
+      /* endorsement3: */ 0x0
+      );
 
-    opportunities[opportunityNumber] = InvestmentOpportunity({
-      opportunityId: opportunityNumber,
-      investee: msg.sender,
-      screener: 0x0,
-      investor: 0x0,
-      opportunityName: _opportunityName,
-      opportunityDescription: _opportunityDescription,
-      currentFunding: 0,
-      fundingTarget: _fundingTarget,
-      investmentScore: 0,
-      opportunityStatus: 1,
-      scoreArray: new uint[](0)
-      /* screeners: 0 */
-      });
+     /* opportunityToOwner[id] = msg.sender; */
+     /* opportunitiesOwned[msg.sender]++; */
 
-      emit OpportunitySnapshot(
-        opportunityNumber,
-        msg.sender,
-        0x0,
-        0x0,
-        _opportunityName,
-        _opportunityDescription,
-        0,
-        _fundingTarget,
-        0,
-        1,
-        /* 0 */
-        opportunities[opportunityNumber].scoreArray.length
-        );
+  // Emit an event
+  emit OpportunitySnapshot(
+    opportunities[opportunityNumber].opportunityId,
+    msg.sender,
+    false,
+    opportunities[opportunityNumber].status,
+    _opportunityName,
+    _opportunityDescription,
+    _fundingTarget,
+    0,
+    0x0,
+    0x0,
+    0x0,
+    0x0
+    );
 }
 
 // get the number of active opportunities
@@ -164,39 +148,106 @@ public view returns(uint){
   return opportunityNumber;
 }
 
+function getAvailableOpportunities() public view returns (uint[]) {
+    //prepare output array
+    uint[] memory opportunityIds = new uint[](opportunityNumber);
+
+    uint numberOfAvailableOpportunities = 0;
+
+    for(uint i = 1; i <= opportunityNumber; i++) {
+      // keep the ID if the article is still for sale
+      /* if(opportunities[i].openForFunding == false){ */
+        opportunityIds[numberOfAvailableOpportunities] = opportunities[i].opportunityId;
+        numberOfAvailableOpportunities++;
+      /* } */
+    }
+
+    // copy the artileIds array into a smaller forSale array
+    uint[] memory forSale = new uint[] (numberOfAvailableOpportunities);
+    for(uint j = 0; j < numberOfAvailableOpportunities; j++) {
+      forSale[j] = opportunityIds[j];
+    }
+
+    return forSale;
+  }
+
 // request investment assessment
-function assessOpportunity(uint _opportunityNumber, uint _ranking) public {
+function assessOpportunity(uint _opportunityNumber) public{
+  opportunities[_opportunityNumber].screeners++;
+
+  if(opportunities[_opportunityNumber].endorsement1 == 0x0) {
+    opportunities[_opportunityNumber].endorsement1 = msg.sender;
+  } else if (opportunities[_opportunityNumber].endorsement2 == 0x0) {
+    opportunities[_opportunityNumber].endorsement2 = msg.sender;
+  } else {
+    opportunities[_opportunityNumber].endorsement3 = msg.sender;
+    string storage status = statusDescriptions[1];
+    opportunities[_opportunityNumber].status = status;
+
+
+  }
+
+/*
   uint newRanking = _ranking;
+  uint totalRankPoints = 0;
+  uint addedRank;
 
   opportunities[_opportunityNumber].scoreArray.push(newRanking);
+  uint scores = opportunities[_opportunityNumber].scoreArray.length;
 
-  newRanking = newRanking - 1;
+  for(uint i = 0; i < scores; i++) {
+    /* newRanking = newRanking + 1; */
 
-  opportunities[_opportunityNumber].scoreArray.push(newRanking);
+    /* addedRank = opportunities[_opportunityNumber].scoreArray[i];
+    totalRankPoints = totalRankPoints.add(addedRank);
+  }
 
-  opportunities[_opportunityNumber].investmentScore = 5;
-
+  opportunities[_opportunityNumber].investmentScore = totalRankPoints; */
+  /* if (scores < 3) { */
+    emit OpportunityEndorsed(msg.sender, opportunities[_opportunityNumber].opportunityName);
+    emit CurrentStatus(opportunities[_opportunityNumber].endorsement1, opportunities[_opportunityNumber].endorsement2, opportunities[_opportunityNumber].endorsement3);
+  /* } else {
+    if(scores == 3 && totalRankPoints >= 9) {
+    opportunities[_opportunityNumber].openForFunding = true;
+    emit CurrentStatus(statusDescriptions[1]);
+  } else {
+    emit CurrentStatus(statusDescriptions[2]);
+  }
+} */
   /* emit OpportunitySnapshot(_opportunityId, _investee, _screener, _investor, _opportunityName, _opportunityDescription, _currentFunding, _fundingTarget, _investmentScore, _opportunityStatus, _scoreArray) */
 }
 
-function getRanking(uint _opportunityNumber) public view returns(uint){
-  /* return opportunities[_opportunityNumber].scoreArray[0]; */
-  return opportunities[_opportunityNumber].investmentScore;
-}
+// buy an article
+  function fundOpportunity(uint _opportunityId) payable public {
+    // Require that there is an article for sellArticle
+    require(opportunityNumber > 0);
 
-// define milestones
+    // check that an article exists
+    require(_opportunityId > 0 && _opportunityId <= opportunityNumber);
 
+    // retrieve the article from the mapping
+    InvestmentOpportunity storage opportunity = opportunities[_opportunityId];
 
+    // Check that the article has not been sold yet
+    require(opportunity.investor == 0x0);
 
+    // don't allow seller to buy own items
+    require(msg.sender != opportunity.investee);
 
-// deploy funding
+    // Check that the value sent is at least that of the Price
+    require(msg.value == opportunity.fundingTarget);
 
+    // keep the buyers information
+    opportunity.investor = msg.sender;
 
+    // the buyer can pay the _seller
+    opportunity.investee.transfer(msg.value);
 
+    string storage status = statusDescriptions[2];
+    opportunity.status = status;
 
-// track progress
-
-
-
+    // log the event
+    emit OpportunityFunded(_opportunityId, opportunity.investee, opportunity.investor, opportunity.opportunityName, opportunity.fundingTarget);
+  }
 }
 
---
+++ src/js/app.js
@@ -0,0 +1,236 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: 0x0,
+  loading: false,
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // initialize web3
+    if(typeof web3 !== 'undefined') {
+      //reuse the provider of the Web3 object injected by Metamask
+      App.web3Provider = web3.currentProvider;
+    } else {
+      //create a new provider and plug it directly into our local node
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    App.displayAccountInfo();
+
+    return App.initContract();
+  },
+
+  displayAccountInfo: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#account').text(account);
+        web3.eth.getBalance(account, function(err, balance) {
+          if(err === null) {
+            $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
+          }
+        });
+      }
+    });
+  },
+
+  initContract: function() {
+    $.getJSON('InvestmentScreen.json', function(investmentScreenArtifact) {
+      // get the contract artifact file and use it to instantiate a truffle contract abstraction
+      App.contracts.InvestmentScreen = TruffleContract(investmentScreenArtifact);
+      // set the provider for our contracts
+      App.contracts.InvestmentScreen.setProvider(App.web3Provider);
+      // listen to listenToEvents
+      App.listenToEvents();
+      // retrieve the opportunity from the contract
+      return App.reloadOpportunities();
+    });
+  },
+
+  reloadOpportunities: function() {
+    // avoid reentry
+    if(App.loading == true) {
+      return;
+    }
+
+    App.loading = true;
+
+    // refresh account information because the balance might have changed
+    App.displayAccountInfo();
+
+    var investmentScreenInstance;
+
+    App.contracts.InvestmentScreen.deployed().then(function(instance) {
+      investmentScreenInstance = instance;
+      return instance.getAvailableOpportunities();
+    }).then(function(opportunityIds) {
+      // retrieve the opportunity placeholder and clear it
+      $('#opportunityRow').empty();
+      // console.log('Right here this is opportunityIds before the loop ' + opportunityIds);
+
+      for (var i = 0; i < opportunityIds.length; i++) {
+        // console.log("this is the length in the loop " + opportunityIds.length);
+        // console.log("i in the loop " + i);
+        opportunityId = opportunityIds[i];
+        // console.log('this is opportunityIds ' + opportunityId);
+        investmentScreenInstance.opportunities(opportunityId.toNumber()).then(function(opportunity){
+          console.log(opportunity[7]);
+          App.displayOpportunity(opportunity[0], opportunity[1], opportunity[4], opportunity[5], opportunity[6], opportunity[3], opportunity[8], opportunity[9], opportunity[10], opportunity[11]);
+        });
+      }
+
+      App.loading = false;
+
+    }).catch(function(err) {
+      console.error(err.message);
+      App.loading = false;
+    });
+  },
+
+  displayOpportunity: function(id, seller, name, description, price, status, investor, endorsement1, endorsement2, endorsement3) {
+    var opportunityRow = $('#opportunityRow');
+    console.log('This is the price ' + endorsement1);
+    var etherPrice = web3.fromWei(price,"ether");
+    var opportunityTemplate = $('#opportunityTemplate');
+    opportunityTemplate.find('.panel-title').text(name);
+    opportunityTemplate.find('.opportunity-description').text(description);
+    opportunityTemplate.find('.opportunity-price').text(etherPrice + " ETH");
+    opportunityTemplate.find('.opportunity-status').text(status);
+    opportunityTemplate.find('.opportunity-endorsement1').text(endorsement1);
+    opportunityTemplate.find('.opportunity-endorsement2').text(endorsement2);
+    opportunityTemplate.find('.opportunity-endorsement3').text(endorsement3);
+    opportunityTemplate.find('.opportunity-investor').text(investor);
+    opportunityTemplate.find('.btn-fund').attr('data-id', id);
+    opportunityTemplate.find('.btn-rank').attr('data-id', id);
+    opportunityTemplate.find('.btn-fund').attr('data-value',etherPrice);
+
+    // check the sellers
+    if(seller == App.account) {
+      opportunityTemplate.find('.opportunity-seller').text("You");
+      opportunityTemplate.find('.btn-fund').hide();
+      opportunityTemplate.find('.btn-rank').hide();
+    } else {
+      opportunityTemplate.find('.opportunity-seller').text(seller);
+      opportunityTemplate.find('.btn-fund').hide();
+      opportunityTemplate.find('.btn-rank').hide();
+      if (status == "Investment needs additional screening" && endorsement1 != App.account && endorsement2 != App.account) {
+        opportunityTemplate.find('.btn-rank').show();
+      }
+      if (status == "Investment has been screened and is ready for funding") {
+        opportunityTemplate.find('.btn-fund').show();
+      }
+    }
+
+    // add this new opportunity to the listenToEvents
+    opportunityRow.append(opportunityTemplate.html());
+
+  },
+
+  createOpportunity: function() {
+    // retrieve the detail of the opportunity
+    var _opportunity_name = $('#opportunity_name').val();
+    var _description = $('#opportunity_description').val();
+    var _price = web3.toWei(parseFloat($('#opportunity_price').val() || 0), "ether");
+
+    if((_opportunity_name.trim() == '') || (_price == 0)) {
+      // nothing to sell
+      return false;
+    }
+
+    App.contracts.InvestmentScreen.deployed().then(function(instance) {
+      return instance.createOpportunity(_opportunity_name, _description, _price, {
+        from: App.account,
+        gas: 500000
+      });
+    }).then(function(result) {
+
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+
+  endorseOpportunity: function() {
+    // retrieve the detail of the opportunity
+    // var _opportunityNumber = 1;
+    var current = $('.btn-rank').data('id');
+
+    console.log("This is the value of current " + current);
+
+    App.contracts.InvestmentScreen.deployed().then(function(instance) {
+      return instance.assessOpportunity(current, {
+        from: App.account,
+        gas: 500000
+      });
+
+    }).then(function(result) {
+      console.log(result);
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+
+  // listen to events triggered by the contract
+  listenToEvents: function() {
+    App.contracts.InvestmentScreen.deployed().then(function(instance) {
+      // opportunity has been created
+      instance.OpportunitySnapshot({}, {}).watch(function(error, event) {
+        if(!error) {
+          $('#events').append('<li class="list-group-item">' + event.args._opportunityName + ' is now for sale!</li>');
+
+        } else {
+          console.error(error);
+        }
+        App.reloadOpportunities();
+      });
+
+      // opportunity has been endorsed
+      instance.OpportunityEndorsed({}, {}).watch(function(error, event) {
+        if(!error) {
+          $('#events').append('<li class="list-group-item">' + event.args._screener + ' has endorsed ' + event.args._opportunityName + '!</li>');
+
+        } else {
+          console.error(error);
+        }
+        App.reloadOpportunities();
+      });
+
+      // opportunity has been funded
+      instance.OpportunityFunded({}, {}).watch(function(error, event) {
+        if(!error) {
+          $('#events').append('<li class="list-group-item">' + event.args._investor + ' has funded ' + event.args._opportunityName + '</li>');
+        } else {
+          console.error(error);
+        }
+        App.reloadOpportunities();
+      });
+    });
+  },
+
+  fundOpportunity: function() {
+    event.preventDefault();
+
+    // Retrieve the opportunity Price
+    var _opportunityId = $(event.target).data('id');
+    var _fundingTarget = parseFloat($(event.target).data('value'));
+
+    App.contracts.InvestmentScreen.deployed().then(function(instance){
+      return instance.fundOpportunity(_opportunityId, {
+        from: App.account,
+        value: web3.toWei(_fundingTarget,"ether"),
+        gas: 500000
+      });
+    }).catch(function(error){
+      console.error(error);
+    });
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
